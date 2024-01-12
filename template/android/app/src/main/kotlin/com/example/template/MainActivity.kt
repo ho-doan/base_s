1cda9613953beb6e4a6e7b92598443992fc6252a
hodp ominext <hodp@ominext.com>
2024-01-12 14:05:40 +0700

    update

--- template/android/app/src/main/kotlin/com/example/template/MainActivity.kt
+++ template/android/app/src/main/kotlin/com/example/template/MainActivity.kt
@@ -1,6 +1,17 @@
 package com.example.template
 
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun onStop() {
+        window.setFlags(WindowManager.LayoutParams.FLAG_SECURE,WindowManager.LayoutParams.FLAG_SECURE)
+        
+        super.onStop()
+    }
+//
+//    override fun onRestart() {
+//        window.clearFlags(WindowManager.LayoutParams.FLAG_SECURE)
+//        super.onRestart()
+//    }
 }
