3e57e9fcf364a7a1f3ef259f49a1af58144cacf6
hodp ominext <hodp@ominext.com>
2024-02-21 16:21:10 +0700

    update

--- domain/lib/core/env/env.dart
+++ domain/lib/core/env/env.dart
@@ -8,3 +8,24 @@ abstract class Env {
   @EnviedField(varName: 'API_ENDPOINT', obfuscate: true)
   static final String apiEndpoint = _Env.apiEndpoint;
 }
+
+@Envied(path: '.env.dev')
+abstract class EnvDev {
+  EnvDev();
+  @EnviedField(varName: 'API_ENDPOINT', obfuscate: true)
+  static final String apiEndpoint = _EnvDev.apiEndpoint;
+}
+
+@Envied(path: '.env.stg')
+abstract class EnvStg {
+  EnvStg();
+  @EnviedField(varName: 'API_ENDPOINT', obfuscate: true)
+  static final String apiEndpoint = _EnvStg.apiEndpoint;
+}
+
+@Envied(path: '.env.test')
+abstract class EnvTest {
+  EnvTest();
+  @EnviedField(varName: 'API_ENDPOINT', obfuscate: true)
+  static final String apiEndpoint = _EnvTest.apiEndpoint;
+}
