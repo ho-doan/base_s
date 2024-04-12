c0fa3db40972f96ea6d0a6145905927236b8ef5c
Ho Doan <doanho.it.dev@gmail.com>
2024-04-12 16:33:45 +0700

    update-firebase

--- template/ios/firebaseScript.sh
+++ template/ios/firebaseScript.sh
@@ -1,4 +1,10 @@
 if [ "$CONFIGURATION" == "Debug-dev" ] || [ "$CONFIGURATION" == "Release-dev" ]; then
   cp Runner/dev/GoogleService-Info.plist Runner/GoogleService-Info.plist
+elif [ "$CONFIGURATION" == "Debug-ctest" ] || [ "$CONFIGURATION" == "Release-ctest" ]; then
+  cp Runner/ctest/GoogleService-Info.plist Runner/GoogleService-Info.plist
+elif [ "$CONFIGURATION" == "Debug-stg" ] || [ "$CONFIGURATION" == "Release-stg" ]; then
+  cp Runner/stg/GoogleService-Info.plist Runner/GoogleService-Info.plist
+elif [ "$CONFIGURATION" == "Debug-prod" ] || [ "$CONFIGURATION" == "Release-prod" ]; then
+  cp Runner/prod/GoogleService-Info.plist Runner/GoogleService-Info.plist
 fi
 
