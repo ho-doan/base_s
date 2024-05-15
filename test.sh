ead4b2c6d6be78a57e0a47aea24db9511585a12e
hodp ominext <hodp@ominext.com>
2024-05-15 15:08:17 +0700

    update

--- test.sh
+++ test.sh
@@ -1,16 +1,17 @@
-# cat ./coverage/_log.txt | awk '{ printf("%s\\\\\\\\n", $0) }' > ./log.txt
-# firstString=$(cat ./log.txt)
-# secondString="\""
-# replaceString="\\\\\""
-# ssb=$(echo "${firstString//$secondString/$replaceString}")
-# echo "{\"logs\":\"${ssb}\"}" > log.json
-
-
-melos exec --scope="*template*" --fail-fast -- flutter test test/app_test.dart --machine &> ./__m.b
-mlgrep=$(grep -Ev '{"|melos|flutter test|1 packages|exit code 1' ./__m.b)
-mlawk=$(echo $mlgrep | awk '{ printf("%s\\\\\\\\n", $0) }')
-secondString="\""
-replaceString="\\\\\""
-ssb=$(echo "${mlawk//$secondString/$replaceString}")
-echo "{\"logs\":\"${ssb}\"}" > ./log_run_app.json
-rm ./__m.b
+mlexit=-1
+if melos exec --scope="*template*" --fail-fast -- flutter test test/app_test.dart --machine;
+then
+    mlexit=0
+else
+    mlexit=1
+    melos exec --scope="*template*" --fail-fast -- flutter test test/app_test.dart --machine &> ./__m.b
+    mlgrep=$(grep -Ev '{"|melos|flutter test|1 packages|exit code 1' ./__m.b)
+    mlawk=$(echo $mlgrep | awk '{ printf("%s\\\\\\\\n", $0) }')
+    secondString="\""
+    replaceString="\\\\\""
+    ssb=$(echo "${mlawk//$secondString/$replaceString}")
+    echo "{\"logs\":\"${ssb}\"}" > ./log_run_app.json
+    rm ./__m.b
+fi
+echo $mlexit
+exit $mlexit
